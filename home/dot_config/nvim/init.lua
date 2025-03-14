defea549c3f223924a5b248da87717edde876110
Simon Truscott <saitora@gmail.com>
2025-03-15 07:49:37 +1100

    Remove tokyonight theme (nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -174,21 +174,6 @@ require('lazy').setup({
       vim.cmd([[colorscheme carbonfox]])
     end,
   },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('tokyonight').setup({
-        style = "night",
-        transparent = true,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = false },
-        },
-      })
-    end,
-  },
 
   -- Icons
   { 'nvim-tree/nvim-web-devicons' },
