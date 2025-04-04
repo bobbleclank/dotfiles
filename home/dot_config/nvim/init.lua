1d52466e16bf76b0b630faa2e6811c9510f2f3b5
Simon Truscott <saitora@gmail.com>
2025-04-05 06:26:09 +1100

    Add tokyodark theme (nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -172,6 +172,24 @@ require('lazy').setup({
       })
     end,
   },
+  {
+    'tiagovla/tokyodark.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+        functions = {},
+        variables = {},
+      },
+    },
+    config = function(_, opts)
+      require('tokyodark').setup(opts)
+    end,
+  },
 
   -- Icons
   { 'nvim-tree/nvim-web-devicons' },
