b2bc1913b222b5031afb94abda0c0641569343ef
Simon Truscott <saitora@gmail.com>
2026-09-12 21:32:01 +1000

    Remove the fuzzy completion mappings (vim, nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -360,9 +360,6 @@ vim.keymap.set('n', '<leader>t', fzf_lua.helptags)
 
 vim.keymap.set('n', '<leader><tab>', fzf_lua.keymaps)
 
-vim.keymap.set('i', '<C-x><C-f>', fzf_lua.complete_path)
-vim.keymap.set('i', '<C-x><C-l>', fzf_lua.complete_line)
-
 -- lsp mappings
 
 -- Global mappings
