ed054de076e081d9a30fa7fe4337181019b4adb8
Simon Truscott <saitora@gmail.com>
2026-09-13 06:35:05 +1000

    Add insert mode to the signature help key mapping (nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -383,7 +383,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gi', fzf_lua.lsp_implementations, opts)
     vim.keymap.set('n', 'gr', fzf_lua.lsp_references, opts)
 
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
 
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
