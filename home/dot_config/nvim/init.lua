1d3e2980f76b4e78b7a2f6695d19a8b1197ce8d0
Simon Truscott <saitora@gmail.com>
2026-09-12 11:43:36 +1000

    Use fzf-lua for LSP references (nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -397,7 +397,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'gc', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
