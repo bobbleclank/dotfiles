3785f4f5ae40b9a7a1ce16a80177b1ec0c36a017
Simon Truscott <saitora@gmail.com>
2026-09-19 06:25:17 +1000

    Map ]q and [q to walk the quickfix list (vim, nvim)

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -311,6 +311,9 @@ vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('x', '<', '<gv')
 vim.keymap.set('x', '>', '>gv')
 
+vim.keymap.set('n', '[q', ':cprev<CR>', { silent = true })
+vim.keymap.set('n', ']q', ':cnext<CR>', { silent = true })
+
 -- fugitive mappings
 vim.keymap.set('n', '<leader>gg', ':Git<CR>', { silent = true })
 vim.keymap.set('n', '<leader>gl', ':Git log<CR>', { silent = true })
